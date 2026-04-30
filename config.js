9ab7e80578b7d6befbd8c0438336ae100f56e5bd
Clint Bird <clintjb@gmail.com>
2026-04-30 21:38:12 +0200

    Cross Repo Fix

--- config.js
+++ config.js
@@ -11,34 +11,34 @@ const posters = [
         author: "Yoda, Jedi Master",
         quote: "Do or do not. There is no try.",
         regulatory: "Photo: Fighting Taco/mitchell. EMOTIONS © GmbH",
-        image: "images/taco.jpg" 
+        image: "https://cdn.jsdelivr.net/gh/clintjb/Motivation-Posters@main/images/taco.jpg" 
     },
     {
         title: "STAY HUMBLE",
         author: "Confucius",
         quote: "Humility is the solid foundation of all virtues.",
         regulatory: "Photo: Carragher + Neville/ynwa. EMOTIONS © GmbH",
-        image: "images/carragher.jpg" 
+        image: "https://cdn.jsdelivr.net/gh/clintjb/Motivation-Posters@main/images/carragher.jpg" 
     },
     {
         title: "RESILIENCE",
         author: "Anonymous",
         quote: "The strongest steel is forged in the fires of a dumpster",
         regulatory: "Photo: Great BW meltdown of Q3 2024/dpagent. EMOTIONS GmbH",
-        image: "images/dumpster.jpg" 
+        image: "https://cdn.jsdelivr.net/gh/clintjb/Motivation-Posters@main/images/dumpster.jpg" 
     },
     {
         title: "MODESTY",
         author: "Matshona Dhliwayo",
         quote: "A flower does not use words to announce its arrival to the world - it just blooms",
         regulatory: "Photo: Regional Sales Lead / Maserati. UNWARRANTED CONFIDENCE © GmbH",
-        image: "images/maserati.jpg" 
+        image: "https://cdn.jsdelivr.net/gh/clintjb/Motivation-Posters@main/images/maserati.jpg" 
     },
     {
         title: "COMMUNICATION",
         author: "Anonymous",
         quote: "A picture isn't always clearer than a thousand words",
         regulatory: "Corkboard #7 / Tommy's IMAGINAYSHUN © LLC. May contain traces of glitter and glue.",
-        image: "images/dataculture.jpg" 
+        image: "https://cdn.jsdelivr.net/gh/clintjb/Motivation-Posters@main/images/dataculture.jpg" 
     }
 ];
